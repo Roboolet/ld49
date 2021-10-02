ac62c8280996166e9a5b07dc134fb4ff50daa16b
Bram <39883637+Roboolet@users.noreply.github.com>
2021-10-02 13:39:24 +0200

    LevelSwitcher
    [+] GameplayScene
    [+] LoadLevel imlementation

--- Assets/Scripts/LevelSwitcher.cs
+++ Assets/Scripts/LevelSwitcher.cs
@@ -5,6 +5,13 @@ using UnityEngine;
 public class LevelSwitcher : MonoBehaviour
 {
     [SerializeField] LevelData[] levels;
+    [SerializeField] Transform vcam;
+
+    [Header("Prefabs")]
+    [SerializeField] GameObject sunPrefab;
+    [SerializeField] GameObject[] planetPrefab;
+
+    List<GameObject> currentSceneObjects = new List<GameObject>();
 
     public void LoadLevel(int i)
     {
@@ -13,13 +20,38 @@ public class LevelSwitcher : MonoBehaviour
 
     void LoadLevel(LevelData data)
     {
+        StartCoroutine(CleanupScene(currentSceneObjects));
+        currentSceneObjects = new List<GameObject>();
+
+        vcam.position += Vector3.right * 50;
+        Vector2 anchor = (Vector2)vcam.position + data.sunOffset;
+
+        currentSceneObjects.Add(Instantiate(sunPrefab, anchor, Quaternion.identity));
+        for(int i = 0; i < data.planets.Length; i++)
+        {
+            PlanetData d = data.planets[i];
+            int rnd = Random.Range(0, planetPrefab.Length);
+
+            GameObject obj = Instantiate(planetPrefab[rnd], anchor + d.position, Quaternion.identity);
+            //obj.GetComponent<Planetoid>().Launch(d.velocity);
+            currentSceneObjects.Add(obj);
+        }
 
     }
 
     LevelData GenerateLevel()
     {
 
-            return new LevelData();
+        return new LevelData();
+    }
+
+    IEnumerator CleanupScene(List<GameObject> obj)
+    {
+        yield return new WaitForSeconds(1);
+        foreach(GameObject go in obj)
+        {
+            Destroy(go);
+        }
     }
 }
 
